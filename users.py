6b49c24df77a112b03b247983c9f18ab82ea3793
Putkuri Heidi M K <heidiput@cubbli22-gold.local>
2023-08-03 14:39:33 +0300

    Correct sql command for login, change check_password_hash, add del session username and role to logout, change sql command in register

--- users.py
+++ users.py
@@ -1,27 +1,31 @@
 from db import db
+import os 
 from flask import session
 from werkzeug.security import check_password_hash, generate_password_hash
 
 def login(username, password):
-    sql = "SELECT id, password FROM users WHERE username=:username"
+    sql = "SELECT password, id, is_leader FROM users WHERE username=:username"
     result = db.session.execute(sql, {"username":username})
     user = result.fetchone()
     if not user:
         return False
-    else:
-        if check_password_hash(user.password, password):
-            session["user_id"] = user.id
-            return True
-        else:
-            return False
+    if not check_password_hash(user[0], password):
+        return False
+    session["user_id"] = user[1]
+    session["user_name"] = username
+    session["user_role"] = user[2]
+    session["csrf_token"] = os.urandom(16).hex()
+    return True
 
 def logout():
     del session["user_id"]
+    del session["user_name"]
+    del session["user_role"]
 
-def register(username, password):
+def register(username, password, is_leader):
     hash_value = generate_password_hash(password)
     try:
-        sql = "INSERT INTO users (username,password) VALUES (:username,:password)"
+        sql = "INSERT INTO users (username,password, is_leader) VALUES (:username,:password,:is_leader)"
         db.session.execute(sql, {"username":username, "password":hash_value})
         db.session.commit()
     except:
