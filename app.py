c87b28710ff2896166d79fc27f79c24ae752d712
Putkuri Heidi M K <heidiput@cubbli22-gold.local>
2023-08-05 13:20:27 +0300

    Add doc string and some corrections on formatting according to pylint on app.py and db.py

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
-from flask import Flask
+"""This is the main file for the application."""
 from os import getenv
-from dotenv import load_dotenv
-
-load_dotenv()
+from flask import Flask
 app = Flask(__name__)
-app.secret_key = getenv('SECRET_KEY')
-
+app.secret_key = getenv("SECRET_KEY")
 import routes
--- db.py
+++ db.py
@@ -1,6 +1,8 @@
-from app import app
-from flask_sqlalchemy import SQLAlchemy
+"""Database configuration"""
 from os import getenv
+from flask_sqlalchemy import SQLAlchemy
+from app import app
 
 app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
