dbdca665ea532035b5a6e5bfddb89b563cf7245a
Putkuri Heidi M K <heidiput@cubbli22-gold.local>
2023-08-05 23:59:12 +0300

    Add create new task functionality

--- routes.py
+++ routes.py
@@ -1,7 +1,9 @@
 """This module contains the routes for the application."""
+from datetime import datetime
 from flask import render_template, request, redirect, session, abort
 import users
 import tasks
+import secrets
 from app import app
 
 @app.route("/")
@@ -18,11 +20,13 @@ def login():
     if request.method == "POST":
         username = request.form["username"]
         password = request.form["password"]
-
-    if users.login(username, password):
-        return redirect("/")
-
-    return render_template("error.html", message="Wrong credentials")
+    
+        if users.login(username, password):
+            # Generate a new CSRF token for the session
+            session["csrf_token"] = secrets.token_urlsafe()
+            return redirect("/")
+        else:
+            return render_template("error.html", message="Invalid username or password")
 
 
 @app.route("/register", methods=["GET", "POST"])
@@ -34,6 +38,7 @@ def register():
         username = request.form["username"]
         password1 = request.form["password1"]
         password2 = request.form["password2"]
+        session["csrf_token"] = secrets.token_urlsafe()
         if password1 != password2:
             return render_template("error.html", message="Passwords do not match")
 
@@ -60,26 +65,51 @@ def logout():
     users.logout()
     return redirect("/")
 
-
+#in progress
 @app.route("/createTask", methods=["GET", "POST"])
 def create_task():
     """Task creation handler"""
+    print("Entered create_task route")  # Add this line
     if request.method == "GET":
         return render_template("createTask.html")
     if request.method == "POST":
-        if session["csrf_token"] != request.form["csrf_token"]:
-            abort(403)
+        print(request.form)  # Print the entire form data
+        try:
+            print(f"CSRF token: {session['csrf_token']}")  # Print the CSRF token
+            print(f"Form CSRF token: {request.form['csrf_token']}")  # Print the CSRF token from the form
+            if session["csrf_token"] != request.form["csrf_token"]:
+                abort(403)
+        except KeyError as ex:
+            print(f"Error accessing CSRF token: {ex}")
+            return render_template("error.html", message="Error accessing CSRF token")
         name = request.form["name"]
         desc = request.form["description"]
-        status = request.form["status"]
-        deadline = request.form["deadline"]
-
-        if tasks.create_task(name, desc, status, deadline):
+        status = "Not started"
+        deadline_str = request.form["deadline"]
+        print(f"Deadline string: {deadline_str}")  # Print the deadline string
+        try:
+            deadline = datetime.strptime(deadline_str, '%Y-%m-%d')
+        except Exception as ex:
+            print(f"Error parsing deadline: {ex}")
+            return render_template("error.html", message="Error parsing deadline")
+        try:
+            creator_id = users.user_id()
+            print(f"Creator id: {creator_id}")  # Print the creator id
+        except Exception as ex:
+            print(f"Error getting creator id: {ex}")
+            return render_template("error.html", message="Error getting creator id")
+        group_id = None
+        assignee_id = creator_id
+        group_id = None
+        if tasks.create_task(name, desc, status, creator_id, assignee_id, group_id, deadline):
             return redirect("/")
-
-
     return render_template("error.html", message="Unknown error")
 
+#in progress
+#@app.route("/createGroup", methods=["GET", "POST"])
+#def create_group():
+#    """Group creation handler"""
+#    print("Entered create_group route")
 
 @app.route("/error")
 def error():
--- tasks.py
+++ tasks.py
@@ -1,7 +1,7 @@
 """Functions to handle tasks."""
 from sqlalchemy import text
 from db import db
-import users
+#import users
 
 def task(task_id):
     "Get task by id"
@@ -9,9 +9,35 @@ def task(task_id):
     result = db.session.execute(sql, {"id":task_id})
     return result.fetchone()
 
-def create_task(name, description, status, deadline):
+def create_task(name, description, status, creator_id, assignee_id, group_id, deadline):
     """Function to create a new task"""
-    sql = text("INSERT INTO tasks (name, description, status, deadline, assignee_id) "\
-            "VALUES (:name, :description, :status, :deadline, :assignee_id) RETURNING id")
-    result = db.session.execute(sql, {"name": name, "description": description, "status": status, "deadline": deadline, "assignee_id": current_user.id})
-    db.session.commit()
+    sql = text("INSERT INTO tasks (name, description, status, creator_id, assignee_id, group_id, deadline) VALUES (:name, :description, :status, :creator_id, :assignee_id, :group_id, :deadline)")
+    try:
+        db.session.execute(sql, {"name":name, "description":description, "status":status, "creator_id":creator_id, "assignee_id":assignee_id, "group_id":group_id, "deadline":deadline})
+        db.session.commit()
+        return True
+    except Exception as e:
+        print(f"Error creating task: {e}")
+        return False
+
+
+#def add_user_to_group(user_id, group_id):
+#    """Function to add user to group"""
+#    sql = text("INSERT INTO group_users (group_id, user_id) VALUES (:group_id, :user_id)")
+#    db.session.execute(sql, {"group_id": group_id, "user_id": user_id})
+#    db.session.commit()
+#    return True
+
+#def add_task_to_group(task_id, group_id):
+#    """Function to add task to group"""
+#    sql = text("INSERT INTO group_tasks (group_id, task_id) VALUES (:group_id, :task_id)")
+#    db.session.execute(sql, {"group_id": group_id, "task_id": task_id})
+#    db.session.commit()
+#    return True
+
+#deleting task
+#def delete_task(task_id):
+#    """Function to delete a task"""
+#    sql = text("DELETE FROM tasks WHERE id=:id")
+#    db.session.execute(sql, {"id":task_id})
+#    db.session.commit()
