4f2a676b67a134c61273538a71fa83913967728f
Putkuri Heidi M K <heidiput@cubbli22-gold.local>
2023-08-06 00:01:02 +0300

    Add get_user_tasks function

--- users.py
+++ users.py
@@ -58,3 +58,16 @@ def username():
 def isleader():
     """returns role from session"""
     return session.get("role", 0)
+
+
+#def get_user_tasks():
+#    """Function to get all tasks for the current user"""
+#    sql = text("SELECT * FROM tasks WHERE assignee_id = :user_id")
+#    result = db.session.execute(sql, {"user_id": current_user.id})
+#    tasks = result.fetchall()
+#    return tasks
+
+#def add_uset_to_group(user_id, group_id):
+#    """Function to add user to group"""
+#    sql = text("INSERT INTO group_users (group_id, user_id) VALUES (:group_id, :user_id)")
+#    db.session.execute(sql, {"group_id": group_id, "user_id": user_id})
